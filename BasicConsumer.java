b6f930bc3770ca025e87a2110dce43d02c4f24f6
Calvin Price <calprice@cisco.com>
2018-06-26 09:50:14 -0700

    Kafka Consumer for two topics

--- BasicConsumer.java
+++ BasicConsumer.java
@@ -14,6 +14,7 @@ import java.util.*;
 
 public class BasicConsumer {
     public static void main(String[] args) {
+        assert (args.length > 0): " No topic specified";
 
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -25,53 +26,86 @@ public class BasicConsumer {
                 ));
 
         //configure the following three settings for SSL Encryption
-        //"/users/subkasi/eloqua_processing/lib/kafka.client.truststore.jks"
         props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
         props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/hdfs/app/GCS_ANA/eloqua_processing/scripts/kafka.client.truststore.jks");
         props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "cisco123");
 
         //configure the following three settings for SSL Authentication
-
-        //"/users/subkasi/eloqua_processing/lib/kafka.client.keystore.jks"
         props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/hdfs/app/GCS_ANA/eloqua_processing/scripts/kafka.client.keystore.jks");
         props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "cisco123");
         props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "cisco123");
 
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "dce-smb-kafka--DSX1");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "dce-smb-kafka--DSX9");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(props);
         TestConsumerRebalanceListener rebalanceListener = new TestConsumerRebalanceListener();
-        consumer.subscribe(Collections.singletonList("mplOrderData"), rebalanceListener);
 
         //String for today's date to be set as the output filename
         DateFormat dateFormat = new SimpleDateFormat("yyMMdd");
         Date date = new Date();
         String todayDate = dateFormat.format(date);
         String fileName = todayDate + ".txt";
+        String targetDataFile;
+        String topic;
 
-        try{
-            PrintWriter out = new PrintWriter(new FileWriter("/hdfs/app/GCS_ANA/gvscsde/projects/calvin_test/" + fileName), true);
-            while (true){
-                ConsumerRecords<byte[], byte[]> records = consumer.poll(1000);
-                for (ConsumerRecord<byte[], byte[]> record : records){
+        //if-else case to catch different topics specified in command line
+        //To add new topic, add another elif case with the requisite topic and targetDataFile
+        if (args[0].equals("mplOrderData")){
+            try{
+                topic = "mplOrderData";
+                targetDataFile = "/hdfs/app/GCS_ANA/gvscsde/projects/order_data/";
 
-                    //System.out.println(record);
+                consumer.subscribe(Collections.singletonList(topic), rebalanceListener);
+                PrintWriter out = new PrintWriter(new FileWriter(targetDataFile + fileName), true);
+                while (true){
+                    ConsumerRecords<byte[], byte[]> records = consumer.poll(1000);
+                    for (ConsumerRecord<byte[], byte[]> record : records){
+                        //System.out.println(record);
 
-                    //Writes content of records to jar file in format of the string below
-                    String str = record.topic() + "|" + record.partition() + "|" + record.offset() + "|"
-                            + record.key() + "|" + record.value();
-                    out.println(str);
+                        //Writes content of records to jar file in format of the string below
+                        String str = record.topic() + "|" + record.partition() + "|" + record.offset() + "|"
+                                + record.key() + "|" + record.value();
+                        out.println(str);
 
-                    //Prints out records that are saved to .txt file
-                    System.out.printf("Received Message topic =%s|partition =%s|offset = %d|key =%s|value =%s\n",
-                            record.topic(), record.partition(), record.offset(), record.key(), record.value());
+                        //Prints out records that are saved to .txt file
+                        System.out.printf("Received Message topic =%s|partition =%s|offset = %d|key =%s|value =%s\n",
+                                record.topic(), record.partition(), record.offset(), record.key(), record.value());
+                    }
+                    consumer.commitSync();
                 }
-                consumer.commitSync();
-            }
-        } catch (IOException e){
+            } catch (IOException e){
                 e.printStackTrace();
+            }
+        } else if (args[0].equals("mplCartData")) {
+            try {
+                topic = "mplCartData";
+                targetDataFile = "/hdfs/app/GCS_ANA/gvscsde/projects/cart_data/";
+
+                //Subscribe to new topic
+                consumer.subscribe(Collections.singletonList(topic), rebalanceListener);
+                PrintWriter out = new PrintWriter(new FileWriter(targetDataFile + fileName), true);
+                while (true){
+                    ConsumerRecords<byte[], byte[]> records = consumer.poll(1000);
+                    for (ConsumerRecord<byte[], byte[]> record : records){
+                        //System.out.println(record);
+
+                        //Writes content of records to jar file in format of the string below
+                        String str = record.topic() + "|" + record.partition() + "|" + record.offset() + "|"
+                                + record.key() + "|" + record.value();
+                        out.println(str);
+
+                        //Prints out records that are saved to .txt file
+                        System.out.printf("Received Message topic =%s|partition =%s|offset = %d|key =%s|value =%s\n",
+                                record.topic(), record.partition(), record.offset(), record.key(), record.value());
+                    }
+                    consumer.commitSync();
+                }
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
